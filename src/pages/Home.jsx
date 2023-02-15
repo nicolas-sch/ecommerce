f5fec24e7e2216237beb12de2e1352c802ac5757
Nicolas Schmidt <nicolasdrivebackup@gmail.com>
2023-02-15 19:00:15 -0300

    build fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,11 @@ import Image from '../assets/img.png';
 export default function Home() {
   return (
     <>
-        <div classNameName="hero">
+        <div className="hero">
             <div className="card bg-dark text-white border-0">
                 <img src={Image} className="card-img" alt="Image Background" height="250px"/>
                 <div className="card-img-overlay d-flex flex-column justify-content-center">
-                    <div classNameName="container">
+                    <div className="container">
                         <h5 className="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
                         <p className="card-text lead fs-2">
                             Check out all the trends
